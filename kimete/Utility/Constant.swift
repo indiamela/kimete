fd364c5873ea8ce4508b9f86624e5420f7c4801e
Taishi Kusunose <taishi.kusunose@gmail.com>
2021-08-16 08:26:50 +0900

    fix-contentView&addFiles

---
+++ kimete/Utility/Constant.swift
@@ -0,0 +1,15 @@
+//
+//  Date+Extension.swift
+//  kimete
+//
+//  Created by Taishi Kusunose on 2021/08/16.
+//
+
+import Foundation
+
+private let itemFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .short
+    formatter.timeStyle = .medium
+    return formatter
+}()
