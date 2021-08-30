589011839300511d82fdfa0d97953dde99f77f96
Taishi Kusunose <taishi.kusunose@gmail.com>
2021-08-31 08:18:23 +0900

    fix-RouletteView

--- kimete/View/RouletteView.swift
+++ kimete/View/RouletteView.swift
@@ -9,52 +9,53 @@ import SwiftUI
 
 struct RouletteView: View {
     @Environment(\.managedObjectContext) private var viewContext
-    
     @FetchRequest(entity: Category.entity(), sortDescriptors: [NSSortDescriptor(keyPath: \Category.id, ascending: true)])
     private var categories: FetchedResults<Category>
+//    @State var category: Category
     @State var newItemText: String = ""
 
     
     var body: some View {
-        VStack {
-            VStack(alignment:.leading){
-                List{
-//                    ForEach(itemArray(categories[0].items), id: \.self) { item in
-//                        Text("\(item.name ?? "")")
-//                    }
+        ZStack{
+            Color.MyTheme.offWhite
+                .edgesIgnoringSafeArea(.all)
+            VStack{
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack{
+                        ForEach(categories){ category in
+                            Button(action: {}) {
+                                Text(category.name ?? "")
+                                    .frame(width: 70, height: 20, alignment: .center)
+                            }
+                            .softButtonStyle(RoundedRectangle(cornerRadius: 20))
+                            .padding()
+                        }
+                    }
                 }
-            }
-            .listStyle(InsetGroupedListStyle())
-            .toolbar {
-                NavigationLink(
-                    destination: CategoryView(),
-                    label: {
-                        Image(systemName: "line.horizontal.3")
-                    })
-            }
-        }
-        
-    }
-    private func addItem(category: Category) {
-        withAnimation {
-            
-            let newItem = Item(context: viewContext)
-            newItem.name = newItemText
-            newItem.timestamp = Date()
-            newItem.category_id = category.id
-            category.addToItems(newItem)
-            
-            do {
-                try viewContext.save()
-            } catch {
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+                Spacer()
+//                List{
+//                    ForEach(itemArray(category.items)){ item in
+//                        Text(item.name ?? "")
+//                    }
+//                }
+//                .cornerRadius(20)
+//                .softOuterShadow(darkShadow: Color.MyTheme.blackShadow, lightShadow: Color.MyTheme.whiteShadow, offset: 10, radius: 20)
+//                .padding()
+//                .background(Color.clear)
+//                .onAppear {
+//                    /// Listビュー表示時に初期データ登録処理を実行
+//                    UITableView.appearance().backgroundColor = UIColor(Color.MyTheme.whiteShadow)
+//                }
             }
         }
-        newItemText = ""
+        .toolbar {
+            NavigationLink(
+                destination: CategoryView(),
+                label: {
+                    Image(systemName: "line.horizontal.3")
+                })
+        }
     }
-    
-
     
     /// NSSet? → [Item]変換
     private func itemArray(_ items: NSSet?) -> [Item] {
@@ -63,13 +64,17 @@ struct RouletteView: View {
             $0.id < $1.id
         }
     }
-}
-
-struct RouletteView_Previews: PreviewProvider {
-    static var previews: some View {
-        NavigationView{
-            RouletteView()
-                .environment(\.managedObjectContext, PersistenceController.shared.container.viewContext)
-        }
+    
+    private func changeCategory() {
+        
     }
 }
+
+//struct RouletteView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        NavigationView{
+//            RouletteView()
+//                .environment(\.managedObjectContext, PersistenceController.shared.container.viewContext)
+//        }
+//    }
+//}
